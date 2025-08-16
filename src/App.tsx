29906b612684df716edb812c47c82ac0dd224d04
JasmineAz <jasminezahraaa15@gmail.com>
2025-08-16 15:21:37 +0700

    branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,26 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+// src/App.tsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import DashboardLayout from "./components/DashboardLayout";
 import Dashboard from "./pages/Dashboard";
 import Laporan from "./pages/Laporan/Laporan";
 import InformasiDesa from "./pages/InformasiDesa";
-import RiwayatPesan from "./pages/RiwayatPesan";
-import LogRealtime from "./pages/LogRealTime";
-import Login from "./pages/Login";
-import Logout from "./pages/Logout";
-import Settings from "./pages/Settings";
+import "./App.css";
+import LaporanDetail from "./pages/LaporanDetail/LaporanDetail";
 
 const App = () => {
-  const isAuthenticated = !!localStorage.getItem("user");
-
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="informasi-desa" element={<InformasiDesa />} />
           <Route path="laporan" element={<Laporan />} />
-          <Route path="riwayat-pesan" element={<RiwayatPesan />} />
-          <Route path="log-realtime" element={<LogRealtime />} />
-          <Route path="settings" element={<Settings />} /> {/* ⬅️ rute baru */}
+          <Route path="laporan">
+            <Route index element={<Laporan />} />
+            <Route path=":ticket" element={<LaporanDetail />} />
+          </Route>
         </Route>
-
-        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
