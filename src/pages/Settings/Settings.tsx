442e0230774ab51570290fb649593a4d8112fb4d
jabiralawfaa <jadealtero@gmail.com>
2025-09-08 21:00:53 +0700

    Revert "Settings V2 & Changelog"
    This reverts commit 41610d9fa4c102751b92beea29185dfe2242141f.

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import SettingsUmum from './Umum/SettingsUmum';
 import SettingsKeamanan from './Keamanan/SettingsKeamanan';
 import SettingsAkun from './Akun/SettingsAkun';
-import SettingsNotifikasi from './Notifikasi/SettingsNotifikasi';
 
 function Settings() {
   return (
@@ -11,7 +10,6 @@ function Settings() {
       <Route path="umum" element={<SettingsUmum />} />
       <Route path="keamanan" element={<SettingsKeamanan />} />
       <Route path="akun" element={<SettingsAkun />} />
-      <Route path="notifikasi" element={<SettingsNotifikasi />} />
     </Routes>
   );
 }
